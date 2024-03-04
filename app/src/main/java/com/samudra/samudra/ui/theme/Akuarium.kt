3c4bee7cd52726086cbbd4b1039a9454afb17e29
daffasamudra <151831010+daffasamudra@users.noreply.github.com>
2024-03-04 15:01:05 +0700

    Step 1: Create some functions

--- app/src/main/java/com/samudra/samudra/ui/theme/Akuarium.kt
+++ app/src/main/java/com/samudra/samudra/ui/theme/Akuarium.kt
@@ -1,3 +1,17 @@
+import java.util.*
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = "pellets"
+    println("Today is $day and the fish eat $food")
+}
+
+fun randomDay(): String {
+    val week = arrayOf("Monday", "Tuesday", "Wednesday", "Thursday",
+        "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
+}
+
 fun main(args: Array<String>) {
-    println("Hello, world!")
+    feedTheFish()
 }
